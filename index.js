7033df87fa2336e32110be874f92a07a06005039
sasha sims <solely.sashasims@gmail.com>
2022-10-15 22:25:15 -0400

    squashed bugs

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 const colors = ["red", "blue", "green"]
 
-document.getElementById("add").addEventListener("onclick", function(e){
+// change onClick to click
+document.getElementById("add").addEventListener("click", function(e){
     const subItem = createSubItem(e)
     document.getElementById("list").appendChild(subItem)
 })
 
 function createDropDown(){
     const dropDown = document.createElement("select")
-    for (let i = 0; i < colors; i++){
-        const option = createElement("option") 
-        option.innerHTML = colors[i]
-        option.value = colors[i]
+    for (let i = 0; i < colors.length; i++){
+        const option = document.createElement("option") 
+        option.setAttribute( 'value' , colors[i])
+        option.text = colors[i]
         dropDown.append(option)
     }
-    dropDown.addEventListener("onchange", function(e){
-        e.target.parent.backgroundColor = e.target.value
+    dropDown.addEventListener("change", function(e){
+        e.target.parentElement.style.backgroundColor = e.target.value
     })
     return dropDown
 }
 
+// add .value to update with a string instead of an object
 function createSubItem(e){
     const subItem = document.createElement("div")
     var subItemValue = document.getElementById("input")
-    subItem.textContent = subItemValue
+    subItem.textContent = subItemValue.value
     const dropDown = createDropDown()
     subItem.appendChild(dropDown)
     subItem.setAttribute("class", "subItem")
